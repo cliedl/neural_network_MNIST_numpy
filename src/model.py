3f0bc43ab1711aa93ae8c7cf429ce85438b52f4c
cliedl <christian.liedl@gmx.net>
2024-01-11 16:54:28 +0100

    Created model module with NN class

---
+++ src/model.py
@@ -0,0 +1,102 @@
+import numpy as np
+from .model_functions import sigmoid, softmax
+
+class NN:
+    def __init__(self,
+                    input_size=784,
+                    hidden1_size=128,
+                    hidden2_size=64,
+                    output_size=10):
+
+        self.dW3 = 0
+        self.db3 = 0
+        self.dW2 = 0
+        self.db2 = 0
+        self.dW1 = 0
+        self.db1 = 0
+
+        self.input_size = input_size
+        self.hidden1_size = hidden1_size
+        self.hidden2_size = hidden2_size
+        self.output_size = output_size
+
+        self.W1, self.W2, self.W3, self.b1, self.b2, self.b3 = self.initialize_wb()
+
+
+    def initialize_wb(self):
+        """
+        Initialize weights and biases for a neural network.
+
+        Parameters:
+        - input_size (int): Number of input features.
+        - hidden1_size (int): Number of neurons in the first hidden layer.
+        - hidden2_size (int): Number of neurons in the second hidden layer.
+        - output_size (int): Number of output neurons (number of classes).
+
+        Returns:
+        - W1, W2, W3 (np.ndarray): 2D arrays for the weights with random values from a standard normal
+            distribution
+        - b1, b2, b3: (np.ndarray): 2D arrays for the bias as zeros
+        # your code here
+        """
+
+        # Initialize weights with random values from a standard normal distribution
+        W1 = np.random.randn(self.input_size, self.hidden1_size)  # size input x hidden1
+        W2 = np.random.randn(self.hidden1_size, self.hidden2_size)  # size hidden1 x hidden2
+        W3 = np.random.randn(self.hidden2_size, self.output_size)  # size hidden2 x output
+
+        # Initialize biases as zeros
+        b1 = np.zeros((1, self.hidden1_size))  # size 1 x hidden1
+        b2 = np.zeros((1, self.hidden2_size))  # size 1 x hidden2
+        b3 = np.zeros((1, self.output_size))  # size 1 x output
+
+        return W1, W2, W3, b1, b2, b3
+
+    def forward_pass(self, x_batch, y_batch, gradient=True):
+        """Forward pass through neural net"""
+        # Forward pass
+        z1 = np.dot(x_batch, self.W1) + self.b1
+        a1 = sigmoid(z1)
+        z2 = np.dot(a1, self.W2) + self.b2
+        a2 = sigmoid(z2)
+        z3 = np.dot(a2, self.W3) + self.b3
+        output = softmax(z3)
+
+        if gradient:
+            # Backpropagation, update derrivatives
+            delta = output - y_batch
+            self.dW3 = np.dot(a2.T, delta)
+            self.db3 = np.sum(delta, axis=0, keepdims=True)
+            delta = np.dot(delta, self.W3.T) * (a2 * (1 - a2))
+            self.dW2 = np.dot(a1.T, delta)
+            self.db2 = np.sum(delta, axis=0, keepdims=True)
+            delta = np.dot(delta, self.W2.T) * (a1 * (1 - a1))
+            self.dW1 = np.dot(x_batch.T, delta)
+            self.db1 = np.sum(delta, axis=0, keepdims=True)
+
+        return output
+
+    def update_weights(self, learning_rate=0.1):
+        # Update weights and biases
+        self.W1 -= learning_rate * self.dW1
+        self.b1 -= learning_rate * self.db1
+        self.W2 -= learning_rate * self.dW2
+        self.b2 -= learning_rate * self.db2
+        self.W3 -= learning_rate * self.dW3
+        self.b3 -= learning_rate * self.db3
+
+
+    def update_weights(self, learning_rate):
+        # Update weights and biases
+        self.W1 -= learning_rate * self.dW1
+        self.b1 -= learning_rate * self.db1
+        self.W2 -= learning_rate * self.dW2
+        self.b2 -= learning_rate * self.db2
+        self.W3 -= learning_rate * self.dW3
+        self.b3 -= learning_rate * self.db3
+
+
+
+
+
+
