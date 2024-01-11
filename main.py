14cf181ce1ddc20e7a845c0f5a0a669213afd949
cliedl <christian.liedl@gmx.net>
2024-01-11 16:54:57 +0100

    Added main: Training network on MNIST dataset

---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from src.model import NN
+from src.data import data_loader, process_data
+
+X_train, y_train, X_test, y_test = data_loader()
+X_train, y_train, X_test, y_test, y_train_onehot = process_data(X_train, y_train, X_test, y_test)
+
+model = NN()
+
+# Training parameters
+learning_rate = 0.1
+epochs = 10
+batch_size = 64
+
+test_accuracy_list = []
+train_accuracy_list = []
+
+for epoch in range(epochs):
+    for i in range(0, len(X_train), batch_size):
+        x_batch = X_train[i:i + batch_size]
+        y_batch = y_train_onehot[i:i + batch_size]
+
+        output = model.forward_pass(x_batch, y_batch)
+
+        model.update_weights(learning_rate)
+
+    # Train accuracy
+    output = model.forward_pass(X_train, y_train, gradient=False)
+    train_predictions = np.argmax(output, axis=1)
+    train_accuracy = np.mean(train_predictions == y_train)
+    train_accuracy_list.append(train_accuracy)
+
+    # Test accuracy
+    output = model.forward_pass(X_test, y_test, gradient=False)
+    test_predictions = np.argmax(output, axis=1)
+    test_accuracy = np.mean(test_predictions == y_test)
+    test_accuracy_list.append(test_accuracy)
+
+    print(
+        f"Epoch {epoch + 1: >2}/{epochs}, Accuracy training set: {train_accuracy:.4f}, Accuracy test set: {test_accuracy:.4f}")
+
